626689d127cec48bbdfa843543b37e370f4f94bb
ksereda <alternativer2005@yandex.ru>
2018-01-08 18:34:32 +0300

    add hibernate config and mysql driver to project

---
+++ src/main/java/com/JavaTests/config/AppConfig.java
@@ -0,0 +1,44 @@
+package com.JavaTests.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+@Configuration
+@PropertySource(value = {"classpath:util.properties"})
+public class AppConfig {
+    @Autowired
+    Environment environment;
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        JdbcDaoImpl jdbcImpl = new JdbcDaoImpl();
+//        jdbcImpl.setDataSource(dataSource());
+//        jdbcImpl.setUsersByUsernameQuery(environment.getRequiredProperty("usersByQuery"));
+//        jdbcImpl.setAuthoritiesByUsernameQuery(environment.getRequiredProperty("rolesByQuery"));
+//        return jdbcImpl;
+//    }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.mysql.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.mysql.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.mysql.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.mysql.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+}
--- src/main/java/com/JavaTests/config/application/WebConfig.java
+++ src/main/java/com/JavaTests/config/application/WebConfig.java
@@ -1,11 +1,17 @@
-package com.JavaTests.application;
+package com.JavaTests.config.application;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.JavaTests.controller", "com.JavaTests.config", "com.jdeath.model", "com.jdeath.service", "com.jdeath.dao"})
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver viewResolver() {
