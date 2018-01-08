f31683fea158afe74586b0105f30ed115639bf1d
ksereda <alternativer2005@yandex.ru>
2018-01-08 18:17:50 +0300

    add webconfig to project structure

---
+++ src/main/java/com/JavaTests/application/WebConfig.java
@@ -0,0 +1,24 @@
+package com.JavaTests.application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setContentType("text/html; charset=utf-8");
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("resources/css/**").addResourceLocations("/resources/css/");
+    }
+}
