19cbf370f2a51cea29929080dfcb087ae194845b
ksereda <alternativer2005@yandex.ru>
2018-01-26 15:17:14 +0300

    edit SecurityConfig

--- src/main/java/com/JavaTests/config/SecurityConfig.java
+++ src/main/java/com/JavaTests/config/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configAuthentication (AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
     }
 
     @Bean
